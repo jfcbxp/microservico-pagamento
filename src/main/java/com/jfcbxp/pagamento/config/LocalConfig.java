cfef4e2dca7c695380ad76169cffcfd2aebea7c7
Joao Felipe <jfcbxp@gmail.com>
2022-05-15 18:42:45 -0300

    projeto iniciado

---
+++ src/main/java/com/jfcbxp/pagamento/config/LocalConfig.java
@@ -0,0 +1,19 @@
+package com.jfcbxp.pagamento.config;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+@Configuration
+@Profile("local")
+public class LocalConfig {
+
+
+
+    @Bean
+    public void startBD() {
+
+    }
+
+}
---
+++ src/test/java/com/jfcbxp/pagamento/PagamentoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jfcbxp.pagamento;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PagamentoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
