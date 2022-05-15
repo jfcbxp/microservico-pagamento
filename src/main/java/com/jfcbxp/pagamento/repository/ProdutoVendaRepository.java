1466730728a7b886aca3b09fa2de0daf2b920524
Joao Felipe <jfcbxp@gmail.com>
2022-05-15 20:42:36 -0300

    interface ProdutoVendaRepository

--- src/main/java/com/jfcbxp/pagamento/repository/ProdutoVendaRepository.java
+++ src/main/java/com/jfcbxp/pagamento/repository/ProdutoVendaRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ProdutoVendaRepository extends JpaRepository<ProdutoVenda,Integer> {
+public interface ProdutoVendaRepository extends JpaRepository<ProdutoVenda, Integer> {
 }
