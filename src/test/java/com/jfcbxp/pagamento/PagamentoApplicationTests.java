ebe348c7fad8352078579f7c4f9b8a78b5d61d9d
Joao Felipe <jfcbxp@gmail.com>
2022-05-16 14:45:03 -0300

    teste classe ResourceExceptionHandlerTest

--- src/test/java/com/jfcbxp/pagamento/PagamentoApplicationTests.java
+++ src/test/java/com/jfcbxp/pagamento/PagamentoApplicationTests.java
@@ -6,9 +6,9 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class PagamentoApplicationTests {
 
-	@Test
-	void main() {
-		PagamentoApplication.main(new String[] {});
-	}
+    @Test
+    void main() {
+        PagamentoApplication.main(new String[]{});
+    }
 
 }
